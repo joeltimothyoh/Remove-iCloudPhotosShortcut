ee045555030999fa36e7eed7901d34aac9b17216
Joel Timothy Oh <joeltimothyoh@live.com>
2017-09-08 18:42:51 +0800

    updated formatting

--- RemoveiCloudPhotosFromThisPC.ps1
+++ RemoveiCloudPhotosFromThisPC.ps1
@@ -1,7 +1,7 @@
 #############################################################################################################################################
-## 1. Running this script will remove 'iCloud Photos' icon from 'This PC' in Windows.													   ##
-## 2. The icon reappears every time iCloud is updated.																					   ##	
-## 3. The following is the full path to the 'iCloud Photos' Registry Key which you can manually remove if necessary:					   ##
+## 1. Running this script will remove 'iCloud Photos' icon from 'This PC' in Windows.                                                      ##
+## 2. The icon reappears every time iCloud is updated.                                                                                     ##   
+## 3. The following is the full path to the 'iCloud Photos' Registry Key which you can manually remove if necessary:                       ##
 ##     'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{F0D63F85-37EC-4097-B30D-61B4A8917118}' ##
 #############################################################################################################################################
 
@@ -15,5 +15,5 @@ Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyCo
 ## Powershell Commands ##
 #########################
 
-#Remove-Item -Path 'HKLM:\SOFTWARE\Test\Key1'							# This removes key named 'Key1'
-#Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Test\Key2' -Name 'hi'		# This removes value named 'hi' in key named 'Key2'
+#Remove-Item -Path 'HKLM:\SOFTWARE\Test\Key1'                           # This removes key named 'Key1'
+#Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Test\Key2' -Name 'hi'        # This removes value named 'hi' in key named 'Key2'
