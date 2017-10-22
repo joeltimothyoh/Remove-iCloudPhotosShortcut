74a6104367f2f5047dc16f1a0f0ece2b1c74ada3
Joel Timothy Oh <joeltimothyoh@live.com>
2017-10-23 06:51:41 +0800

    Updated script

--- RemoveiCloudPhotosFromThisPC.ps1
+++ RemoveiCloudPhotosFromThisPC.ps1
@@ -5,7 +5,59 @@
 ##     'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{F0D63F85-37EC-4097-B30D-61B4A8917118}' ##
 #############################################################################################################################################
 
-Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{F0D63F85-37EC-4097-B30D-61B4A8917118}'
+$key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{F0D63F85-37EC-4097-B30D-61B4A8917118}'
+
+function foundkey
+{
+    $found = $false
+    try {
+        $item = Get-Item -Path $key -ErrorAction Stop    # Convert exception from non-terminating to terminating
+        if ($item) {
+            $found = $true
+        }        
+    } catch {
+        $e = $_.Exception.GetType().Name            
+        if ($e) {
+            Write-Host "Finding the registry key failed." -ForegroundColor Yellow
+        }      
+    }
+    $found
+}
+
+function removekey
+{
+    try {
+        Remove-Item -Path $key -ErrorAction Stop    # Convert exception from non-terminating to terminating
+        $success = $true
+    } catch {
+        $e = $_.Exception.GetType().Name
+        if ($e -eq 'SecurityException') {
+            Write-Host "Removing the registry key failed because removing a registry key requires elevated permissions. Run this script with administrative rights." -ForegroundColor Yellow
+        }
+        if ($e -eq 'ItemNotFoundException') {
+            Write-Host "Removing the registry key failed because it could not be found." -ForegroundColor Yellow
+        }      
+    }
+    if ($success) {
+        $true
+    }
+    $false
+}
+
+Write-Host "Finding key..." -ForegroundColor Cyan
+if (foundkey) {
+    Write-Host "Found key." -ForegroundColor Cyan
+    Write-Host "Attempting to remove key..." -ForegroundColor Cyan
+    if (removekey) {
+        Write-Host "Removed key." -ForegroundColor Cyan
+        Write-Host "Successfully removed iCloud Photos from This PC." -ForegroundColor Green
+    } else {
+        Write-Host "Failed to remove iCloud Photos from This PC."
+    }
+} else {
+    Write-Host "Registry key for removing iCloud Photos from This PC could not be found. Doing nothing."
+}
+    
 
 ###############################################################################################################
 ## For more information, see: https://www.eightforums.com/general-support/35046-remove-icloud-photos-pc.html ##
