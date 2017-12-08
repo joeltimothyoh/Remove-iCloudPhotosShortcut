187b49ffa919eba0cc25141a7322d3775f454c8d
Joel Timothy Oh <joeltimothyoh@live.com>
2017-12-09 02:05:09 +0800

    Improved check, status messages, and layout. Added main function and documentation. Updated readme

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # Remove-iCloud-Photos-Shortcut
 Removes 'iCloud Photos' listed under 'This PC' in File Explorer in Windows.
 
+## Description
 The 'iCloud Photos' shortcut is automatically created every time iCloud for Windows is installed or updated. Removal of the shortcut is not possible within File Explorer, but can be achieved by removing the shortcut's associated registry key.
 
 The script will:
 1. Find the registry key
 2. Attempt to remove the registry key
 
-Upon a successful removal of the registry key, a message of success will be printed. 'iCloud Photos' should no longer be listed under 'This PC' in File Explorer.
+Upon successful removal of the registry key, a message of success will be printed. 'iCloud Photos' should no longer be listed under 'This PC' in File Explorer.
 
-## Usage:
+## Usage
 Simply manually run, or set the script to run on a schedule.
 
-Note: Editing Windows registry requires elevated permissions. Run the script with administrative rights.
+Note: Editing the registry on Windows requires elevated permissions. Run the script with administrative rights.
 
-.\RemoveiCloudPhotosFromThisPC.ps1
+## Example
+`.\Remove-iCloud-Photos-Shortcut.ps1`
--- Remove-iCloud-Photos-Shortcut.ps1
+++ Remove-iCloud-Photos-Shortcut.ps1
@@ -10,55 +10,90 @@ The 'iCloud Photos' shortcut is automatically created every time iCloud for Wind
 
 #>
 
-$key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{F0D63F85-37EC-4097-B30D-61B4A8917118}'
-
-function foundkey
-{
-    $found = $false
+function foundkey {
     try {
-        $item = Get-Item -Path $key -ErrorAction Stop                       # ErrorAction Stop converts this exception from non-terminating to terminating
-        if ($item) {
-            $found = $true
-        }
-    } catch {
-        $e = $_.Exception.GetType().Name            
-        if ($e) {
-            Write-Host "Finding the registry key failed." -ForegroundColor Yellow
-        }
+        # Get the registry key
+        $item = Get-Item -Path $key -ErrorAction Stop       # ErrorAction Stop converts this exception from non-terminating to terminating
+
+        # Set flag to true if registry key is found
+        $found = $true
     }
-    $found
-}
 
-function removekey
-{
-    try {
-        Remove-Item -Path $key -ErrorAction Stop                               
-        $success = $true
-    } catch {
+    # Handle potential exceptions
+    catch {
         $e = $_.Exception.GetType().Name
-        if ($e -eq 'SecurityException') {
-            Write-Host "Removing the registry key failed because removing a registry key requires elevated privileges. Run this script with administrative rights." -ForegroundColor Yellow
-        }
         if ($e -eq 'ItemNotFoundException') {
-            Write-Host "Removing the registry key failed because it could not be found." -ForegroundColor Yellow
+            Write-Host "The registry key could not be found." -ForegroundColor Yellow
         }
     }
-    if ($success) {
+
+    # Return true if registry key is found
+    if ($found) {
         $true
     }
+
+    # Assume false
     $false
 }
 
-Write-Host "Finding key..." -ForegroundColor Cyan
-if (foundkey) {
-    Write-Host "Found key." -ForegroundColor Cyan
-    Write-Host "Attempting to remove key..." -ForegroundColor Cyan
-    if (removekey) {
-        Write-Host "Removed key." -ForegroundColor Cyan
-        Write-Host "Successfully removed iCloud Photos from This PC." -ForegroundColor Green
-    } else {
-        Write-Host "Failed to remove iCloud Photos from This PC."
+function removekey {
+    try {
+        # Remove the registry key
+        Remove-Item -Path $key -ErrorAction Stop        # ErrorAction Stop converts this exception from non-terminating to terminating
+
+        # Set flag to true if registry key is removed
+        $success = $true
+    } 
+
+    # Handle potential exceptions
+    catch {
+        $e = $_.Exception.GetType().Name
+        if ($e -eq 'SecurityException') {
+            Write-Host "The registry key could not be removed due to insufficient priveleges. Run the script with administrative rights." -ForegroundColor Yellow
+        }
+        if ($e -eq 'ItemNotFoundException') {
+            Write-Host "The registry key could not be found." -ForegroundColor Yellow
+        }
     }
-} else {
-    Write-Host "Registry key for removing iCloud Photos from This PC could not be found. Doing nothing."
+
+    # Return true if registry key is removed
+    if ($success) {
+        $true
+    }
+
+    # Assume false
+    $false
 }
+
+function Remove-iCloud-Photos-Shortcut {
+
+    # Define path to registry key
+    $key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{F0D63F85-37EC-4097-B30D-61B4A8917118}'
+
+    Write-Host "Finding key..."
+
+    # Begin process of removal of the registry key, printing appropriate status messages
+    # Attempt to find the registry key
+    if (foundkey) {
+        Write-Host "Found key." -ForegroundColor Cyan
+        Write-Host "Removing key..."
+
+        # Attempt to remove the registry key
+        if (removekey) {
+            Write-Host "Removed key." -ForegroundColor Cyan
+            Write-Host "Successfully removed iCloud Photos shortcut from This PC." -ForegroundColor Green
+        }
+        # When the registry key failed to be removed
+        else {
+            Write-Host "Failed to remove iCloud Photos shortcut from This PC."
+        }
+    }
+    # When the registry key failed to be found
+    else {
+        Write-Host "The registry key for iCloud Photos shortcut could not be found. Doing nothing."
+    }
+
+}
+
+# Call function
+Remove-iCloud-Photos-Shortcut
