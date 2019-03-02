00443e2fd5dd7b8aed4c8b37965f27bd94f10e64
Joel Timothy Oh <joeltimothyoh@live.com>
2019-03-03 00:29:12 +0800

    Add param() to main function

--- Remove-iCloudPhotosShortcut.ps1
+++ Remove-iCloudPhotosShortcut.ps1
@@ -107,6 +107,7 @@ function Remove-QuickAccessItem {
 }
 
 function Remove-iCloudPhotosShortcut {
+    Param()
     # iCloud Photos This PC shortcut's associated registry key
     [string]$REGISTRY_KEY_PATH = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{F0D63F85-37EC-4097-B30D-61B4A8917118}'
 
