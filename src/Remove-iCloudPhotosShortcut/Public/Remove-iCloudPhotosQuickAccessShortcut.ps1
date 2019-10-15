fcacab7c30e6663f454e1258495d5abd76c59651
Joel Timothy Oh <joeltimothyoh@live.com>
2019-10-15 20:01:41 +0800

    Add windows os version check for at least '10.0.10240' to run Remove-iCloudPhotosQuickAccessShortcut

--- src/Remove-iCloudPhotosShortcut/Public/Remove-iCloudPhotosQuickAccessShortcut.ps1
+++ src/Remove-iCloudPhotosShortcut/Public/Remove-iCloudPhotosQuickAccessShortcut.ps1
@@ -1,6 +1,10 @@
 function Remove-iCloudPhotosQuickAccessShortcut {
     [CmdletBinding()]
     param()
+    if ([System.Environment]::OSVersion.Version -lt [Version]'10.0.10240') {
+        Write-Error -Exception ([System.Management.Automation.RuntimeException]::New("The function is not supported on this version of Windows."))
+        return
+    }
     # iCloud Photos Quick access shortcut's item path
     [string]$QA_ITEM_PATH = '::{20D04FE0-3AEA-1069-A2D8-08002B30309D}\::{F0D63F85-37EC-4097-B30D-61B4A8917118}'
     try {
