75e7cfbd8f167cf2e56de3cf7fd972089fe7db7d
Joel Timothy Oh <joeltimothyoh@live.com>
2019-10-15 08:02:21 +0800

    Remove Private dir in script module file

--- src/Remove-iCloudPhotosShortcut/Remove-iCloudPhotosShortcut.psm1
+++ src/Remove-iCloudPhotosShortcut/Remove-iCloudPhotosShortcut.psm1
@@ -8,12 +8,10 @@ Set-StrictMode -Version Latest
 $script:MODULE = @{}
 $script:MODULE['BASE_DIR'] = $PSScriptRoot
 $script:MODULE['PUBLIC_DIR'] = Join-Path $script:MODULE['BASE_DIR'] 'Public'          # Module public functions
-$script:MODULE['PRIVATE_DIR'] = Join-Path $script:MODULE['BASE_DIR'] 'Private'        # Module private functions
 $script:MODULE['HELPERS_DIR'] = Join-Path $script:MODULE['BASE_DIR'] 'helpers'        # Module helpers
 
 # Load vendor, Public, Private, classes, helpers
 Get-ChildItem -Path "$($script:MODULE['PUBLIC_DIR'])\*.ps1" | % { . $_.FullName }
-Get-ChildItem -Path "$($script:MODULE['PRIVATE_DIR'])\*.ps1" | % { . $_.FullName }
 Get-ChildItem -Path "$($script:MODULE['HELPERS_DIR'])\*.ps1" | % { . $_.FullName }
 
 # Export Public functions
